6100fce6ff53e12628a5341cce5bef690daa7066
Bao <bao-tran.cong-huyen-ton-nu.1@ens.etsmtl.ca>
2020-07-08 11:22:24 -0400

    Add exercice 3.3

--- index.js
+++ index.js
@@ -45,6 +45,17 @@ app.get('/info', (req, res) => {
     res.send(`<div> Phonebook has info for ${totalPeople} people <br /> ${date}</div>`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if(person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
